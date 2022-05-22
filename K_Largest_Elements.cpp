a1607cd512449566e9205991926dbcc3ee62d33f
Atharva Morankar <90545519+Atharva-Morankar-09@users.noreply.github.com>
2022-05-22 12:40:55 +0530

    Add files via upload

---
+++ K_Largest_Elements.cpp
@@ -0,0 +1,36 @@
+
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main()
+{
+    int n,k;
+    cout<<"Enter the size of array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter elements of array : "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    } 
+    cout<<"Enter the value of k : ";
+    cin>>k;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for(int i=0;i<n;i++)
+    {
+        pq.push(arr[i]);
+        if(pq.size()>k)
+        {
+            pq.pop();
+        }
+    }
+    
+    cout<<"The K largest elements are : ";
+    while(!pq.empty())
+    {
+        cout<<pq.top()<<" ";
+        pq.pop();
+    }
+   return 0; 
+}
---
+++ Sort_a_K_Sorted_array.cpp
@@ -0,0 +1,37 @@
+
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main()
+{
+    int n,k;
+    cout<<"Enter the size of array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of array :"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    priority_queue<int, vector<int>,greater<int>> pq;
+    cout<<"Enter value of k : ";
+    cin>>k;
+    cout<<"The sorted array is : ";
+    for(int i=0;i<n;i++)
+    {
+        pq.push(arr[i]);
+        if(pq.size()>k)
+        {
+            cout<<pq.top()<<" ";
+            pq.pop();
+        }
+    }
+    while(!pq.empty())
+    {
+        cout<<pq.top()<<" ";
+        pq.pop();
+    }
+   return 0; 
+}
